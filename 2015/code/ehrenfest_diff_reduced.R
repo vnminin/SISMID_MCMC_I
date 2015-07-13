cd322b914089fb9d6723e7079a028d90828c1637
vnminin <vminin@uw.edu>
2015-07-12 22:59:30 -0700

    changes to styles of R scripts

--- 2015/code/ehrenfest_diff_reduced.R
+++ 2015/code/ehrenfest_diff_reduced.R
@@ -4,23 +4,23 @@
 
 ## this function randomly draws a new state of the Ehrenfest model
 ## One of your tasks is to finish writing this function
-next.state = function(cur.state, num.mol){
+next_state = function(cur.state, num.mol){
   return.value=NULL
   
   return(return.value)
 }
 
 ## set the number of molecules and the number of iterations
-my.num.mol = 100
-sim.size = 1000
+my_num_mol = 100
+sim_size = 1000
 
 ## initialize the chain by drawing the initial state uniformly at random from all possible states. R function `sample()' will be handy.
-my.draws = numeric(sim.size)
+my_draws = numeric(sim_size)
 #my.draws[1] = FINISH AND UNCOMMENT THIS LINE
 
 ## run the Markov chain
 
-for (i in 2:sim.size){
+for (i in 2:sim_size){
   ## use next.state function to evolve the Markov chain one step at a time
   
   #my.draws[i] = FINISH AND UNCOMMENT THIS LINE
@@ -28,13 +28,13 @@ for (i in 2:sim.size){
 
 ## plot the chain
 
-plot(1:sim.size, my.draws, type="l", ylab="Ehhenfest State", xlab="Time Step")
+plot(1:sim_size, my.draws, type="l", ylab="Ehhenfest State", xlab="Time Step")
 
 ## get the "time averages"
 
-mean(my.draws)
-var(my.draws)
+mean(my_draws)
+var(my_draws)
 
 ## get the "space averages"
-my.num.mol/2
-my.num.mol/4
+my_num_mol/2
+my_num_mol/4
--- 2015/code/import_sampl_reduced.R
+++ 2015/code/import_sampl_reduced.R
@@ -3,15 +3,14 @@
 ## last update: 07/12/2015
 
 ## define a threshold value and number of Monte Carlo samples
-my.const = 4.5
-sim.size = 10000
+my_const = 4.5
+sim_size = 10000
 
 ## true probability of interest
-(true.prob = pnorm(my.const,lower.tail=FALSE))
+(true_prob = pnorm(my_const,lower.tail=FALSE))
 
 
 ## naive Monte Carlo estimate
-(naive.est = mean(ifelse(rnorm(sim.size) > my.const,1,0)))
 
 ## Your task: create a naive and an importance sampling estimate of the normal tail
 ## probability. To generate realizations from the shifted exponential
