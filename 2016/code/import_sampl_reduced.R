097609495c36c5ed4acaf139e3c8713621995eea
vnminin <vminin@uw.edu>
2016-07-13 13:04:25 -0700

    played with comments formatting

--- 2016/code/import_sampl_reduced.R
+++ 2016/code/import_sampl_reduced.R
@@ -12,4 +12,12 @@ sim_size = 10000
 
 ## naive Monte Carlo estimate
 
-## Your task: create a naive and an importance sampling estimate of the normal tail probability. To generate realizations from the shifted exponential use `rexp()` to generate regular exponentials and then add my.const to them. Also, remember that you don't have to code the formula for the normal density, because it is available via `dnorm()'. If you finish early, get Monte Carlo errors for naive and important sampling schemes. 
+## Your task: create a naive and an importance sampling 
+## estimate of the normal tail probability. 
+## To generate realizations from the shifted exponential 
+## use `rexp()` to generate regular exponentials and 
+## then add my.const to them. Also, remember that you 
+## don't have to code the formula for the normal 
+## density, because it is available via `dnorm()'. 
+##If you finish early, get Monte Carlo errors for 
+## naive and important sampling schemes. 
