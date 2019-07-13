771da3caf7493a542ebdb29d0abd4314bf862805
vnminin <vminin@uci.edu>
2019-07-13 11:43:51 -0700

    started adding 2019 files

---
+++ 2019/code/chain_hierarchical_reduced.R
@@ -0,0 +1,191 @@
+#########################################################################
+# This script provides routines to draw samples from the joint posterior 
+# distribution of the two parameters ('alpha' and 'beta') in the hierarchical 
+# chain binomial model for the measles outbreak data. In addition to 'alpha' and
+# 'beta', the model unknowns include the household-specific escape probabilities 
+# and the unknown chain identity in households with outbreak size 3.  
+#
+#  (n1^j,n11^j,n111^j,n12^j)| q_j ~ Multinom(q_j^2,2q_j^2p_j,2q_jp_j^2,p_j^2)
+#  q_j| alpha, beta               ~ Beta(alpha,beta)
+#  (al,be)                        ~(alpha+beta)^(-5/2) (cf. the lecture notes)
+#
+# Last updated July 6, 2019
+##########################################################################
+
+propose.par = function(cur.par,prop.unif,delta.w){
+# This function samples a new candidate value symmetrically 
+# around the current value of the parameter iterate
+  
+  return(cur.par + (0.5-prop.unif)*delta.w)
+  }
+
+log.likelihood = function(my.al,my.be,q.vec){
+# This function calculates the value of the log-likelihood 
+# function of the two parameters (alpha and beta), based on 
+# the current iterates of the household-specific escape probabilities
+#
+# INPUT my.al = parameter alpha
+#       my.be = parameter beta
+#       q.vec = a vector of household-specific escape probabilities
+
+ return(sum(log(dbeta(q.vec,my.al,my.be))))
+ }
+
+chain_hierarchical = function(mcmc.size){
+# This is the main sampling routine. The data, the parameters of the 
+# prior distributon and the 'tuning parameters' (= the proposal window widths)
+# are hard-wired in this code. In addition to the two model parameters (alpha and beta), 
+# the algorithm draws samples of the 334 escape probabilities and 
+# the unknown realisations of transmission chains in the 275 households with 
+# the final number infected as 3. In its current form, only the samples of alpha and beta 
+# are returned as output from this function.
+#
+# INPUT mcmc.size = the number of MCMC iterations to be sampled
+#
+# OUTPUT chain_hierarchical = the MCMC sample of parameters 'alpha' and 'beta'
+#                             of the Beta distribution for household specific
+#                             escape probabilities
+
+# Tuning parameters: the widths of the proposal windows
+delta.alpha = 0.25 # for parameter alpha
+delta.beta  = 0.40 # for parameter beta
+
+# Step (a): Reserve space for the MCMC samples
+al      = rep(0,mcmc.size)
+be      = rep(0,mcmc.size)
+q       = matrix(0,nrow=mcmc.size,ncol=334)
+n111    = matrix(0,nrow=mcmc.size,ncol=275)
+
+# Step (b): Initialize
+cur.alpha = 1
+cur.beta  = 1
+
+al[1]    = cur.alpha
+be[1]    = cur.beta
+q[1,]    = rep(0.5,334)
+n111[1,] = rbinom(275,1,2*0.5/(2*0.5+1))
+
+# The observations
+n1  = 34   # frequency of chain 1
+n11 = 25   # frequency of chain 1->1
+N3  = 275  # frequency of chains of size 3 (i.e., 1->2 or 1->1->1)
+
+# Draw MCMC samples
+for (i in 2:mcmc.size){
+
+ ####################################################
+ # Step (c): Update the household-specific escape probabilities
+ #           through Gibbs steps
+ ####################################################
+ # households with chain 1
+ for (j in 1:n1){
+   q[i,j] = ...   # YOUR TASK IS TO COMPLETE THIS. USE THE 'rbeta' FUNCTION
+   }
+
+ # households with chain 1->1
+ for (j in 1:n11){
+   q[i,j+n1] = ...# YOUR TASK IS TO COMPLETE THIS. USE THE 'rbeta' FUNCTION
+   }
+
+ # households with chain 1->1->1 or 1->2
+ # N.B. In household j, the (current iterate of the) number 
+ #      of escapes is equal to n111[i-1,j]
+ for (j in 1:275){
+   q[i,j+n1+n11] = ... # YOUR TASK IS TO COMPLETE THIS. USE THE 'rbeta' FUNCTION
+   }
+
+
+ #############################################################
+ # Step (d) Update the household-specific 'frequencies' of chain 1->1->1
+ #          through Gibbs steps
+ #############################################################
+ for (j in 1:N3){ 
+ 
+  # Draw a new iterate of n111 from its full conditional distribution
+  n111[i,j] = rbinom(1,1,2*q[i,j+n1+n11]/(2*q[i,j+n1+n11]+1))
+  }
+
+ #################################################################
+ # Step (e) Update parameter 'alpha' in a Metropolis-Hastings step
+ #################################################################
+
+ # Propose a new value for parameter alpha
+ new.alpha = propose.par(cur.alpha,runif(1),delta.alpha)
+
+ # If the proposed value if within the range of the parameter 
+ if (new.alpha >0){
+
+   # The log acceptance ratio = the log likelihood ratio   
+   log.likeratio.alpha = log.likelihood(new.alpha,cur.beta,q[i,]) - log.likelihood(cur.alpha,cur.beta,q[i,]) 
+
+   # The log acceptance ratio = the log-posterior ratio (since a symmetric proposals are being made)
+   log.acc.alpha = log.likeratio.alpha -(5/2)*log(new.alpha+cur.beta) + (5/2)*log(cur.alpha+cur.beta)
+ 
+   # Metropolis step for alpha: determine whether the proposal is accepted
+     if (log(runif(1)) < log.acc.alpha){
+     cur.alpha = new.alpha
+     }
+   }
+
+ #################################################################
+ # Step (f): Update parameter 'beta' in a Metropolis-Hastings step
+ #################################################################
+
+ # Propose a new value for parameter beta
+ new.beta  = propose.par(cur.beta,runif(1),delta.beta)
+
+ # If the proposed value is withing the range of the parameter
+ if (new.beta > 0) { 
+  
+   # The log likelihood ratio
+   log.likeratio.beta = log.likelihood(cur.alpha,new.beta,q[i,]) - log.likelihood(cur.alpha,cur.beta,q[i,]) 
+ 
+   # The log acceptance ratio = the log-posterior ratio (since a symmetric proposals are being made)
+   log.acc.beta = log.likeratio.beta -(5/2)*log(cur.alpha+new.beta) + (5/2)*log(cur.alpha+cur.beta)
+ 
+   # Metropolis step for beta: determine whether the proposal is accepted
+   if (log(runif(1))< log.acc.beta){
+      cur.beta = new.beta
+      }
+   }   
+
+ ######################################################### 
+ # Store the current iterates of parameters alpha and beta
+ #########################################################
+ al[i] = cur.alpha
+ be[i] = cur.beta 
+ 
+}
+
+# The output: the MCMC samples
+return(list(al=al,be=be))
+
+}
+
+# Call the main routine
+mcmc.size = 10000
+mcmc.sample = chain_hierarchical(mcmc.size)
+mcmc.al = mcmc.sample$al
+mcmc.be = mcmc.sample$be
+
+# Discard burn-in samples
+burn.in = 2000
+mcmc.al = mcmc.al[(burn.in+1):mcmc.size]
+mcmc.be = mcmc.be[(burn.in+1):mcmc.size]
+
+# Plot the marginal posterior distributions of parameters alpha and beta
+par(mfrow=c(1,2))
+hist(mcmc.al,xlab='alpha',main='')
+hist(mcmc.be,xlab='beta',main='')
+
+# Plot the joint posterior distribution of alpha and beta
+par(mfrow=c(1,1))
+plot(mcmc.al,mcmc.be,xlab='alpha',ylab='beta')
+
+# Plot the posterior distribution of the expected escape probability
+hist(mcmc.al/(mcmc.al+ mcmc.be),breaks=20,xlab='expected escape probability',main='')
+
+# Plot the posterior predictive distribution of the escape probability
+qpost = rbeta((mcmc.size-burn.in),mcmc.al,mcmc.be)
+hist(qpost,main="posterior predictive distribution of the escape probability",cex.main=1,xlab="predictive q",breaks=20)
+
---
+++ 2019/code/checkmodel_reduced.R
@@ -0,0 +1,85 @@
+  checkmodel = function(mcmc.sample,mcmc.burnin){
+# Last updated July 6, 2019
+#
+# This function draws numerical samples from the joint posterior 
+# predictive distribution of frequencies n1, n11, n111 and n12.
+# Each individual sample is based on one realisation (sample) from the posterior
+# distribution of the escape probability q. The escape probability is assumed to 
+# be the same across all 334 households.
+#
+# The function outputs the mean of the predictive distribution, based on the 
+# sample. In addition, the joint predictive distribution of frequencies n1 and n11
+# is plotted. 
+#
+# Ref: the lecture notes.
+#
+# INPUT mcmc.sample = the output (= MCMC samples) from program mychainGibbs.R 
+#                     (or chainGibbs.R)
+#       mcmc.burnin = the number of MCMC samples to be discarded
+#
+# OUTPUT expfreq = the posterior predictive expectations of the 
+#                  four chain frequencies (n1,n11,n111,n12)
+
+# The number of MCMC samples in the input
+mcmc.size = length(mcmc.sample$q)
+
+# Discard the burn-in samples and retain the rest of the samples for 
+# the escape probability q
+q = mcmc.sample$q[(mcmc.burnin+1):mcmc.size]
+
+# The number of samples retained (= the size of the sub-sample)
+mcmc.subsamplesize = mcmc.size-mcmc.burnin
+
+# Reserve space for the predictive frequencies
+Npred = matrix(0,mcmc.subsamplesize,4)
+
+# Calculate the posterior predictive chain probabilities for
+# each retained MCMC sample. This produces vectors of length 
+# 'mcmc.subsamplesize': 
+P1   =  q^2
+P11  =  2*(1-q)*q^2   
+P111 =  2*(1-q)^2*q
+P12  = (1-q)^2
+
+# Sample from the posterior predictive distribution of chain frequencies
+# with the total  number of chains as 334.
+for (k in 1:mcmc.subsamplesize){
+  Npred[k,] = ... # YOUR TASK TO WRITE THE RIGHT-HAND SIDE OF THE EXPRESSION.
+                  # Use the 'rmultinom' function.
+  } 
+
+# Plot the posterior predictive distribution for frequencies n1 and n11
+par(mfrow=c(1,1))
+plot(0:60,0:60,type="n",xlab='n1',ylab='n11',cex.lab=2)           
+points(jitter(Npred[,1],2.5),jitter(Npred[,2],2.5),pch='.',cex=3) 
+ 
+# The actually observed value
+points(34,25,col='red')
+abline(v=34,col='red',lwd=0.25)
+abline(h=25,col='red',lwd=0.25)
+
+# The posterior predictive expected frequnecies of the four chain types
+expfreq = round(334*c(mean(P1),mean(P11),mean(P111),mean(P12)))
+
+# Return the posterior predictive expected frequencies
+print("The posterior predictive expected frequencies of the four chain types")
+expfreq
+
+return(expfreq)
+
+}
+
+# Draw a sample from the posterior distribution of q 
+mcmc.sample = chainGibbs(5000,1,1)
+
+## or
+# mcmc.sample = mychainGibbs(34,25,275,5000,1,1)
+
+# Check the model fit
+checkmodel(mcmc.sample,mcmc.burnin=500)
+
+
+
+
+
+
